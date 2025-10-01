ca9eadd6bea1877f42560d30a0038542ee5ab5d9
muhammedgamal12 <45471321+muhammedgamal12@users.noreply.github.com>
2025-10-01 15:33:22 +0300

    Delete Dependency_Injection directory

--- Dependency_Injection/src/com/example/Draw2D.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Draw2D {
-    public void drawShape(Shape shape){
-        System.out.println("Drawing a 2d for  " +shape.getClass().getSimpleName());
-
-    }
-}
--- Dependency_Injection/src/com/vehicle/Vehicles.java
+++
@@ -1,8 +0,0 @@
-package com.vehicle;
-
-import java.sql.SQLException;
-
-public interface Vehicles {
-
-    public void saveVehicle(Vehicles vehicles) throws SQLException;
-}
